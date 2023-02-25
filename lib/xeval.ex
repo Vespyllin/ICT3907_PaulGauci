dc279430d2e6ef7bfe454ed44b2a6b1d0b32db67
loveless <esoryx@outlook.com>
2023-02-25 18:18:29 +0100

    modify function

---
+++ lib/xeval.ex
@@ -0,0 +1,9 @@
+defmodule Xeval do
+  def sample_fn(thing) do
+    thing + 2
+  end
+
+  def hello do
+    :world
+  end
+end
---
+++ test/xeval_test.exs
@@ -0,0 +1,8 @@
+defmodule XevalTest do
+  use ExUnit.Case
+  doctest Xeval
+
+  test "greets the world" do
+    assert Xeval.hello() == :world
+  end
+end
