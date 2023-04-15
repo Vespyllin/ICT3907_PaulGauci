bd0ddaf863e8a2c5a47d3f5c0b0458e198294d9f
loveless <esoryx@outlook.com>
2023-04-15 05:47:49 +0200

    benchmark v1

--- run.exs
+++ run.exs
@@ -1,12 +1,4 @@
-# Code.require_file("./resources/calc_client.ex")
-# Code.require_file("./lib/weaver.ex")
+Code.require_file("benchmark.ex")
 
-src_path = "./resources/calc_server.ex"
-dest_path = "./resources"
-
-# :hml_eval.compile('prop_add_rec.hml', [{:outdir, './_build/dev/lib/xeval/ebin'}, :v])
-
-Weaver.weave(src_path, dest_path, true)
-
-# pid = Demo.CalcServer.start(0)
-# Demo.CalcClient.rpc(pid, {:add, 1, 2})
+pids = Benchmark.run_benchmark(100_000, 2)
+IO.inspect(pids)
